cad7601b80b3fb55bd84169faf08b621ee695ce5
Ashish Gupta <agupta90@slb.com>
2023-06-05 16:38:09 +0530

    added main class

---
+++ src/main/java/VehicleManagementSystem.java
@@ -0,0 +1,9 @@
+import vehiclesystem.Vehicle;
+
+public class VehicleManagementSystem {
+    public static void main(String[] args) {
+        // Create instances of Car, Motorcycle, and Truck objects
+        Vehicle vehicle = new Vehicle();
+        System.out.println("Hello World!");
+    }
+}
